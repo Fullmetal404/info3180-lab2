a866712ade16e479fab94bd2860bda8366fe712e
Fullmetal404 <gardnerj874@gmail.com>
2025-02-23 00:48:20 -0500

    Added an image, HTML and styled the webpage

--- app/views.py
+++ app/views.py
@@ -1,6 +1,27 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
+import json
 
+# JSON data as a dictionary
+user_data = {
+    "name": "Mary Jane",
+    "username": "@mjane",
+    "location": "Kingston, Jamaica",
+    "joined": "2018-01-01",
+    "posts": "7",
+    "following": "100",
+    "followers": "250"
+}
+
+# Accessing values
+name = user_data["name"]
+username = user_data["username"]
+location = user_data["location"]
+joinedraw = user_data["joined"]
+posts = user_data["posts"]
+following = user_data["following"]
+followers = user_data["followers"]
 
 ###
 # Routing for your application.
@@ -15,8 +36,21 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name=name)
 
+"""returns the date formatted as Month, Year (e.g. Feb, 2021)"""
+def format_date_joined(date_string):
+    date_obj = datetime.strptime(date_string, "%Y-%m-%d")  # Convert string to datetime object
+    return "Joined " + date_obj.strftime("%b, %Y")  # Format as "Month, Year"
+
+
+joined = format_date_joined(joinedraw)
+
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html', name=name, username=username, location=location,\
+     joined=joined, posts=posts, following=following, followers=followers)
 
 ###
 # The functions below should be applicable to all Flask apps.
